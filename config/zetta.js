7ecb4ba661eb3258e0cb2da4a22b57a02515f4c1
Gianluca Cornetta <gianluca.cornetta@gmail.com>
2017-08-01 20:56:27 +0000

    added zetta server

---
+++ config/zetta.js
@@ -0,0 +1,15 @@
+var winston = require ('winston')
+var zetta = require('zetta')
+var device  = require ('../lib/fablab/driver/scout')
+var Machine = require('../models/machine')
+
+Machine.checkIfMachineConfigured( function (err, machine) {
+  if (err) throw err
+  zetta()
+    .name('machine-wrapper')
+    .link ('http://pigateway.local:1337')
+    .use (device, machine.name, machine.type)
+    .listen(1337, function () {
+       winston.info('@zetta: Server started on piwrapper');
+    })
+  })
