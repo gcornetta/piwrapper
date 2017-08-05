384722dc87e1a536585dcbb75b9c4e8ec201ba02
Gianluca Cornetta <gianluca.cornetta@gmail.com>
2017-08-05 18:42:00 +0000

    created custom logger with same look and feel

--- config/winston.js
+++ config/winston.js
@@ -1,5 +1,7 @@
 var path = require('path');
 var winston = require('winston');
+var strftime = require('strftime');
+var colors = require('colors');
 
 var _getTimestamp = function(){
 	var d = new Date();
@@ -15,19 +17,36 @@ var _getTimestamp = function(){
 //path to log file
 var logfile = path.resolve(__dirname, '../logs/' + _getTimestamp().toString() + '.log');
 
+const timestamp = function() {
+    return strftime('%b-%d-%Y %H:%M:%S ', new Date()).green;
+};
+
+const logFormatter = function(options) {
+    // Return string will be passed to logger.
+
+    return options.timestamp() + ('[' + options.level +']').blue +
+        ` `+ (options.message ? options.message : ``).blue +
+        (options.meta && Object.keys(options.meta).length ?
+            `\n\t`+ JSON.stringify(options.meta) : `` ).blue;
+};
+
+
 winston.remove(winston.transports.Console);
 winston.add(winston.transports.Console, {
             level: 'info',
-            timestamp: true,
+            timestamp: timestamp,
+            formatter: logFormatter,
             colorize: true
 });
 winston.add(winston.transports.File, {
             level: 'info',
             filename: logfile,
-            timestamp: true,
+            timestamp: timestamp,
+            formatter: logFormatter,
             maxsize: 5242880, //5MB
             maxFiles: 5,
-            colorize: true
+            colorize: true,
+	    json: false
         });
 
 //conflicts with File transport
